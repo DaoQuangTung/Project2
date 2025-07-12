fa94352f509208d783ddf25f4bf8ec49a6511b59
DaoQuangTung <daoquangtung3@gmail.com>
2025-07-12 19:44:23 +0700

    update project

--- src/main/java/com/javaweb/api/BuildingAPI.java
+++ src/main/java/com/javaweb/api/BuildingAPI.java
@@ -1,24 +1,16 @@
 package com.javaweb.api;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.javaweb.model.BuildingDTO;
-import com.javaweb.model.ErrorResponseDTO;
 import com.javaweb.service.BuildingService;
 
 import customexceptions.FieldRequiredException;
@@ -29,10 +21,8 @@ public class BuildingAPI {
 	private BuildingService buildingService;
 
 	@GetMapping(value = "/api/building/")
-	public List<BuildingDTO> getBuilding(@RequestParam(name = "name", required = false) String name,
-										@RequestParam(name = "districtId", required = false) Long districtId,
-										@RequestParam(name = "typeCode", required = false) List<String> typeCode) {
-		List<BuildingDTO> result = buildingService.findAll(name, districtId);
+	public List<BuildingDTO> getBuilding(@RequestParam Map<String, Object> params) {
+		List<BuildingDTO> result = buildingService.findAll(params);
 		return result;
 	}
 
--- src/main/java/com/javaweb/service/BuildingService.java
+++ src/main/java/com/javaweb/service/BuildingService.java
@@ -1,11 +1,10 @@
 package com.javaweb.service;
 
 import java.util.List;
+import java.util.Map;
 
 import com.javaweb.model.BuildingDTO;
-import com.javaweb.repository.entity.BuildingEntity;
 
 public interface BuildingService {
-	List<BuildingDTO> findAll(String name, Long districtId);
-
+	List<BuildingDTO> findAll(Map<String, Object> params);
 }
